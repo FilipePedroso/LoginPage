7e8c4caad7fe923638ec468db822da77d3ab61ed
FilipePedroso <58314525+FilipePedroso@users.noreply.github.com>
2022-08-02 17:48:45 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,23 +12,42 @@
     <body>
         <?php
             $label = 'Login';
-            if (isset($_POST['enviar'])) {
+            if(isset($_POST['enviar'])) {
                 $nome = $_POST['nome'];
                 $senha = $_POST['senha'];
-                if ($nome == 'filipe' && $senha == '123') {
-                    $label = "Welcome $nome";
-                    }
+                if($nome == 'filipe' && $senha == '123') {
+                    $label = 'Bem Vindo Filipe';
+                    header ("location: http://WWW.facebook.com/");
+                }
                 else {
-                    $label = 'Unregistered';
+                    $label = 'Não registrado';
+                }
+                if(isset($_POST['lembrar']) == true) {
+                    session_start();
+                    setcookie('nome','filipe', time() + (60 * 60 * 24* 365),'/');
+                    setcookie('senha','123', time() + (60 * 60 * 24 * 365),'/');
+                    setcookie('checkbox','checked', time() + (60 * 60 * 24 * 365),'/');
+                }
+                else {
+                    session_start();
+                    setcookie('nome','filipe', time() - (60 * 60 * 24* 365),'/');
+                    setcookie('senha','123', time() - (60 * 60 * 24 * 365),'/');
+                    setcookie('checkbox','', time() + (60 * 60 * 24 * 365),'/');
+
                 }
             }
+
         ?>
-        <form method='post' class='login'>
-            <h1 id='titulo'><?php echo $label?></h1>
-            <input class='text' type='text' name='nome' placeholder='User' value='' style='margin-top: 40px;'>
-            <input class='text'type='password' name='senha' placeholder='Password'>
-            <input class='button' type='submit' name='enviar' value='Login'>
-            <input class='button' type='submit' name='registrar' value='Register'>
+        <form method='post' id='login'>
+            <h1><?php echo $label; ?></h1>
+            <input class='text' name='nome' type='text' placeholder='User' value='<?php echo @$_COOKIE['nome'];?>' style='margin-top: 40px;'>
+            <input class='text'name='senha' type='password' placeholder='Password' value='<?php echo @$_COOKIE['senha'];?>'>
+            <div id='lembrarbox'>
+                <input type='checkbox' name='lembrar' value='true' <?php echo @$_COOKIE['checkbox'];?>>
+                <label for='lembrar'>Lembrar Login</label>
+            </div>
+            <input class='button' name='enviar' type='submit' value='Login' >
+            <button class='button'>Register</button>
         </form>
 
     </body>
